cd62aeb5f6bd1e67e3e51b86ce3a8bef9a733bdd
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-05-17 18:07:29 +0800

    ok

---
+++ tuitui_weichat/model/AdMaterial.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var connect_url = require('../conf/proj.json').mongodb;
+var db = mongoose.createConnection(connect_url);
+var autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(db);
+
+var AdMaterialSchema = new Schema({
+    novelLink: String,
+    imgList: {
+        url: String,
+        sign: String,
+        weight: String
+    }
+});
+
+AdMaterialSchema.plugin(autoIncrement.plugin, {
+    model: 'AdMaterial',
+    field: 'id',
+    startAt: 1,
+    incrementBy: 1
+});
+
+var AdMaterialModel = db.model('AdMaterial', AdMaterialSchema);
+module.exports = AdMaterialModel;
+
