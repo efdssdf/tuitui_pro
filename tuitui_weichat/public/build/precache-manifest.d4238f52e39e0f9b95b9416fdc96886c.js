c42734f048ef7f87eea19931e54c922f47234b68
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-06-20 16:15:29 +0800

    ok

---
+++ tuitui_weichat/public/build/precache-manifest.d4238f52e39e0f9b95b9416fdc96886c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20a514b69a836c3012446973b516daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2785d6ba511cbf6977a9",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "26e98c546728ff2c664a",
+    "url": "/static/css/main.e3ab1be2.chunk.css"
+  },
+  {
+    "revision": "2785d6ba511cbf6977a9",
+    "url": "/static/js/2.9578a2a3.chunk.js"
+  },
+  {
+    "revision": "26e98c546728ff2c664a",
+    "url": "/static/js/main.93a5135b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e709b51a6a8f8bf2561d3a85c66267c3",
+    "url": "/static/media/bg.e709b51a.png"
+  }
+]);
