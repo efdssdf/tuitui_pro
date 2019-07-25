1e586ead10a2ed5c756b8e5dc4510701d599c754
Administrator <lionel@buzzy.cc>
2019-07-25 16:09:07 +0800

    1

--- tuitui_weichat/util/kouling.js
+++ tuitui_weichat/util/kouling.js
@@ -1,11 +1,11 @@
 var request = require('request');
 
 function getkouling() {
-	var klurl = "https://m.tb.cn/h.e7jal8s";
-	var kltext = "免费分享返利机器人";
-	var tpwdpic = "https://gw.alicdn.com/tfs/TB1c.wHdh6I8KJjy0FgXXXXzVXa-580-327.png";
-	var rq_url ="https://api.open.21ds.cn/apiv1/createtkl?apkey=7603dffc-e95d-e284-9c07-de897e0fbcc5&"
-	+"klurl="+klurl+"&kltext="+kltext+"&tpwdpic="+tpwdpic
+	var klurl = encodeURIComponent("https://m.tb.cn/h.e7jal8s");
+	var kltext = encodeURIComponent("免费分享返利机器人");
+	var tpwdpic = encodeURIComponent("https://gw.alicdn.com/tfs/TB1c.wHdh6I8KJjy0FgXXXXzVXa-580-327.png");
+	var rq_url ="https://api.open.21ds.cn/apiv1/createtkl?apkey=7603dffc-e95d-e284-9c07-de897e0fbcc5"
+	+"&klurl="+klurl+"&kltext="+kltext+"&tpwdpic="+tpwdpic
 	request.get(rq_url,function(err,response){
 		if(err){
 			console.log(err)
