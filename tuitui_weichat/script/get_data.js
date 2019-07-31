935cfa62307f970a48724e2ca6049355922bb5ad
Administrator <lionel@buzzy.cc>
2019-07-31 16:03:32 +0800

    1

--- tuitui_weichat/script/get_data.js
+++ tuitui_weichat/script/get_data.js
@@ -3,7 +3,7 @@ var wechat_util = require('../util/get_weichat_client.js')
 
 async function getData(code) {
 	var client = await wechat_util.getClient(code)
-	client.getUserRead('2019-07-31','2019-07-31',function(err,res){
+	client.getUserRead('2019-07-30','2019-07-30',function(err,res){
 		console.log(err,res)
 	})
 }
