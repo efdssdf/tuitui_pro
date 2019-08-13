1e1753a67053dfef2e4ba3e3335b6211db4cccbe
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-08-13 18:01:12 +0800

    ok

--- tuitui_weichat/public/service-worker.js
+++ tuitui_weichat/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30fb0701caf046ea37218daef2f649f8.js"
+  "/precache-manifest.aeb3ed2aa1169e2e6c6a54416797121d.js"
 );
 
 self.addEventListener('message', (event) => {
