dc3962ef6a38e6a87b5c2931d1e5ed800fedca94
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-08-23 16:27:53 +0800

    修改Model引入路径

--- tuitui_weichat/routes/customer.js
+++ tuitui_weichat/routes/customer.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CustomerModel = require('../model/Custome.js')
+const CustomerModel = require('../model/Customer.js')
 
 router.post('/', async (req, res, next) => {
   let { account, password, belongTo } = req.body;
