199d5ee91ad57338926d6a684d8e8c1d672c26b9
weblixin <weblixin@163.com>
2019-09-03 18:24:24 +0800

    ok

---
+++ tuitui_weichat/public/precache-manifest.02352fde75e99839b07b20e94edd56e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb62b5590c95c2061f6e5c22eb16da76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "660b5792498eb473f021",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "c9e8a3154a9a32fadf0f",
+    "url": "/static/css/main.b07050ee.chunk.css"
+  },
+  {
+    "revision": "660b5792498eb473f021",
+    "url": "/static/js/2.086d544c.chunk.js"
+  },
+  {
+    "revision": "c9e8a3154a9a32fadf0f",
+    "url": "/static/js/main.958400a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e709b51a6a8f8bf2561d3a85c66267c3",
+    "url": "/static/media/bg.e709b51a.png"
+  },
+  {
+    "revision": "e33471629415c521f7ab25608d016801",
+    "url": "/static/media/login_bg.e3347162.png"
+  }
+]);
