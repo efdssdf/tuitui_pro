a529bb074c3562010561e0fac93f5ab971701a89
zhangzicong6 <zhangzicong6@gmail.com>
2020-02-13 23:29:30 +0800

    1

--- tuitui_weichat/script/tengwen_data.js
+++ tuitui_weichat/script/tengwen_data.js
@@ -8,39 +8,47 @@ const PlatformData = require("../model/PlatformData");
 const md5 = crypto.createHash('md5');
 
 const secret = "n3BtjDGlSL23wk4vbd2kj8dboaOZHMi8";
-const time = new Date().getTime();
-const end = new Date(now_time).setSeconds(0,0);
-const last_time = now_time - 60*1000;
-const start = new Date(last_time).setSeconds(0,0);
 
+/*
 let rule = new schedule.RecurrenceRule();
 rule.second = 10;
 let j = schedule.scheduleJob(rule, function () {
     getPlatformData()
 });
+*/
 
 async function getPlatformData() {
-    let result = await Platform.find();
+    let now_time = new Date().getTime();
+    let end = new Date(now_time).setSeconds(0,0);
+    let last_time = now_time - 60*1000;
+    let start = new Date(last_time).setSeconds(0,0);
+
+    let result = await Platform.find({platform:2});
     if(!result.length) {
         return
     } else {
         for(let i = 0; i < result.length; i ++) {
             let resultItem = result[i];
-            if(resultItem.platform === 2) {
-                let sign = resultItem.seruid + secret + time;
-                let qs = {
-                    reg_start_time: parseInt(start/1000),    
-                    reg_end_time: parseInt(end/1000),    
-                    seruid: resultItem.seruid,
-                    time,     
-                    sign: md5.update(sign).digest("hex")
-                };
-                getTengwenData(qs, 1)
-            }
-            
+            //if(resultItem.platform === 2) {
+                exec_req(resultItem,start,end)
+            //}
         }
     }
-}       
+} 
+
+function exec_req(resultItem,start,end){
+    let sign = resultItem.seruid + secret + time;
+    let qs = {
+        reg_start_time: parseInt(start/1000),    
+        reg_end_time: parseInt(end/1000),    
+        seruid: resultItem.seruid,
+        time,     
+        sign: md5.update(sign).digest("hex")
+    };
+    getTengwenData(qs, 1)
+   
+    //getTengwenOrder(qs, 1)
+}      
 
 function getTengwenData(qs, page) {
     return new Promise((resolve, reject) => {
@@ -72,16 +80,13 @@ async function mapUserDataSource(dataSource, qs) {
         let {ip, ua, wx_gzhopenid, regtime, isfollow, id} = dataItem;
         let uni_ip_h_ua = handleIpAndUa(ip, ua);
         let updateData = await PlatformData.findOneAndUpdate({uni_ip_h_ua}, {
-            regtime, 
+            regtime : regtime*1000, 
             wx_openid: wx_gzhopenid,   
             wx_ua: ua, 
             wx_id: id,
             wx_platfrom: 2,
             isfollow
         }, {upsert: true});
-        if(updateData) {
-            getTengwenOrder(qs, 1)
-        }
     }
 }
 
@@ -118,15 +123,35 @@ async function mapOrderDataSource(dataSource) {
     for(let i = 0; i < dataSource.length; i ++) {
         let dataItem = dataSource[i];
         let {atime, amount, userid, ispay} = dataItem;
-        await PlatformData.findOneAndUpdate({wx_gzhopenid}, {
+        let pd = await PlatformData.findOneAndUpdate({wx_openid : wx_gzhopenid}, {
             wx_userid: userid, 
             amount, 
-            order_time: atime,
+            order_time: atime*1000,
             ispay
         });
+        if(pd.td_url){
+            let ad_cb_url = 'https://ad.toutiao.com/track/activate/?link='
+                            +item.td_url+'&event_type=2'
+            await rp(ad_cb_url)
+        }
     }
+}
+
+function rp(url){
+    return new Promise((resolve,reject) => {
+        request(url,function(err, res){
+            if(err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
+    })
 }
 
 function handleIpAndUa(ip, ua) {
     return (ip + ua.substring(0,ua.indexOf(')',ua.indexOf(')')+1)+1));
 }
+
+
+exec_req({seruid:''},,)
