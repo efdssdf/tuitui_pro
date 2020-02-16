f51f0840dc0d538f06f2ff592f6cd4a021e48d47
zhangzicong6 <zhangzicong6@gmail.com>
2020-02-16 16:23:59 +0800

    1

--- tuitui_weichat/script/tengwen_data.js
+++ tuitui_weichat/script/tengwen_data.js
@@ -196,8 +196,8 @@ function handleIpAndUa(ip, ua) {
 }
 
 let test = () =>{
-    //2020-02-16 15:00:03
-    let now_time = new Date().getTime();
+    //
+    let now_time = new Date('2020-02-16 15:00:03').getTime();
     let end = new Date(now_time).setSeconds(0,0);
     let last_time = end -4*60*1000;
     let start = new Date(last_time).setSeconds(0,0);
