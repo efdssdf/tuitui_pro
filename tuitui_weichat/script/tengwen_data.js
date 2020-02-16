c3f694aa0c6c91c7979cc1371a7603c53893979d
zhangzicong6 <zhangzicong6@gmail.com>
2020-02-16 16:53:21 +0800

    1

--- tuitui_weichat/script/tengwen_data.js
+++ tuitui_weichat/script/tengwen_data.js
@@ -8,11 +8,11 @@ const PlatformData = require("../model/PlatformData");
 const secret = "n3BtjDGlSL23wk4vbd2kj8dboaOZHMi8";
 
 
-let rule = new schedule.RecurrenceRule();
+/*let rule = new schedule.RecurrenceRule();
 rule.second = 50;
 let j = schedule.scheduleJob(rule, function () {
     getPlatformData()
-});
+});*/
 
 
 async function getPlatformData() {
@@ -199,12 +199,12 @@ function handleIpAndUa(ip, ua) {
 
 let test = () =>{
     //
-    let now_time = new Date('2020-02-16 15:00:03').getTime();
+    let now_time = new Date('2020-02-16 16:45:03').getTime();
     let end = new Date(now_time).setSeconds(0,0);
     let last_time = end -4*60*1000;
     let start = new Date(last_time).setSeconds(0,0);
-    exec_user_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
-    //exec_order_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
+    //exec_user_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
+    exec_order_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
 }
 
-//test()
+test()
