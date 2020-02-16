2a18693b58d74803138329c3b9e9b9fe804b3d44
zhangzicong6 <zhangzicong6@gmail.com>
2020-02-16 16:03:31 +0800

    1

--- tuitui_weichat/script/tengwen_data.js
+++ tuitui_weichat/script/tengwen_data.js
@@ -198,11 +198,11 @@ function handleIpAndUa(ip, ua) {
 }
 
 let test = () =>{
-    let now_time = new Date('2020-02-16 15:00:05').getTime();
+    let now_time = new Date().getTime();
     let end = new Date(now_time).setSeconds(0,0);
     let last_time = end -4*60*1000;
     let start = new Date(last_time).setSeconds(0,0);
-    exec_user_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
+    exec_order_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
 }
 
 test()
