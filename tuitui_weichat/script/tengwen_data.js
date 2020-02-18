186f57416f36761e49c454a716fbef4019df2fd3
zhangzicong6 <zhangzicong6@gmail.com>
2020-02-18 09:56:43 +0800

    1

--- tuitui_weichat/script/tengwen_data.js
+++ tuitui_weichat/script/tengwen_data.js
@@ -203,8 +203,8 @@ let test = () =>{
     let end = new Date(now_time).setSeconds(0,0);
     let last_time = end -24*60*60*1000;
     let start = new Date(last_time).setSeconds(0,0);
-    //exec_user_req({seruid:'22327'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
-    exec_order_req({seruid:'22328'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
+    exec_user_req({seruid:'22328'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
+    //exec_order_req({seruid:'22328'}, parseInt(start/1000), parseInt(end/1000), parseInt(Date.now()/1000))
 }
 
 test()
