611fbe2ae894e46ff226370c2e7633b2bc8e5423
efdssdf <efdssdf@gmail.com>
2020-03-13 13:23:35 +0800

    'proj'

--- tuitui_weichat/util/ali_oss_util.js
+++ tuitui_weichat/util/ali_oss_util.js
@@ -1,22 +1,24 @@
 let OSS = require('ali-oss');
+let accessKeyId = require('../conf/proj.json').accessKeyId;
+let accessKeySecret = require('../conf/proj.json').accessKeySecret;
 
 let client = new OSS({
-  region: 'oss-cn-beijing',
-  accessKeyId: 'LTAI4Fet8ckC13LicyVEtgDR',
-  accessKeySecret: 'DowKHUfM1A6czBHU8cIQSX5nNk2jRn',
-  bucket: 'tuiguang-td'
+    region: 'oss-cn-beijing',
+    accessKeyId: accessKeyId,
+    accessKeySecret: accessKeySecret,
+    bucket: 'tuiguang-td'
 });
 
-let upload = async (name,filename) =>{
-	try{
-	    //object-name可以自定义为文件名（例如file.txt）或目录（例如abc/test/file.txt）的形式，实现将文件上传至当前Bucket或Bucket下的指定目录。
-	    let result = await client.put(name, filename);
-	    //console.log('------result-------')
-	    //console.log(result);
-	    return 'https://tuiguang-td.oss-cn-beijing.aliyuncs.com/'+name;
-	} catch (e) {
-	    console.log(e);
-	}
+let upload = async (name, filename) => {
+    try {
+        //object-name可以自定义为文件名（例如file.txt）或目录（例如abc/test/file.txt）的形式，实现将文件上传至当前Bucket或Bucket下的指定目录。
+        let result = await client.put(name, filename);
+        //console.log('------result-------')
+        //console.log(result);
+        return 'https://tuiguang-td.oss-cn-beijing.aliyuncs.com/' + name;
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports.upload = upload;
