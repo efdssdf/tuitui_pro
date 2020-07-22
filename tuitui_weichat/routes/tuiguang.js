3e14516fa86e326510cacb52bcc8b48c32aea092
root <lionel@buzzy.cc>
2020-07-22 20:57:00 +0800

    1

--- tuitui_weichat/routes/tuiguang.js
+++ tuitui_weichat/routes/tuiguang.js
@@ -355,13 +355,13 @@ async function up_td(temp){
   let td_url = decodeURIComponent(temp.td_url);
   //console.log('----回传头条用户关注------',td_url);
   let event_type =19;
-  /*
+  
   // 穿山甲测试
-  let chshjs= ['wxfxmswl1379','wxfxmswl1240','wxfxmswl1245']
+  let chshjs= ['wxfxmswl1237','wxfxmswl1379']
   if( chshjs.indexOf(temp.seruid) != -1 ){
     event_type = 3
   }
-  */
+  
   if(td_url.indexOf('?')!=-1){
     let urls = td_url.split('adid')
     if(urls.length==3){
